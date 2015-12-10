779ad9a523d1860f13abf7bcf76e1c13f5d962c3
Kerolos <keronakhla@gmail.com>
2015-12-09 23:25:45 -0800

    Add objc-c header briding header

---
+++ MindfulLifeProject/UIViewController+CheckinViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+CheckinViewController.h
+//  MindfulLifeProject
+//
+//  Created by Kerolos Nakhla on 12/9/15.
+//  Copyright © 2015 Christopher Queen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (CheckinViewController)
+
+@end
